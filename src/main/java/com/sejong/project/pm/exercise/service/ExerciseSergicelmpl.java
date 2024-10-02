697facc2e672adab337e2abbee087642662635e6
yongsik <wkdrndydtlr@naver.com>
2024-10-02 16:52:46 +0900

    exception example

--- src/main/java/com/sejong/project/pm/exercise/service/ExerciseSergicelmpl.java
+++ src/main/java/com/sejong/project/pm/exercise/service/ExerciseSergicelmpl.java
@@ -4,6 +4,7 @@ import com.sejong.project.pm.exercise.Exercise;
 import com.sejong.project.pm.exercise.dto.ExerciseRequest;
 import com.sejong.project.pm.exercise.dto.ExerciseResponse;
 import com.sejong.project.pm.exercise.repository.ExerciseRepository;
+import com.sejong.project.pm.global.handler.MyExceptionHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.sejong.project.pm.global.exception.codes.ErrorCode.NOT_VALID_ERROR;
 
 @Service
 @Slf4j
@@ -29,6 +32,11 @@ public class ExerciseSergicelmpl implements ExerciseService{
         List<ExerciseResponse.searchResultDto> searchResultLlist = new ArrayList<>();
 
         List<Exercise> searchFromDb = exerciseRepository.findByExercisieNameStartingWith(searchexercise.exerciseName());
+
+        if(searchFromDb == null || searchFromDb.isEmpty()){
+             throw new MyExceptionHandler(NOT_VALID_ERROR);
+        }
+
         for(Exercise e : searchFromDb){
             searchResultLlist.add(
                     new ExerciseResponse.searchResultDto(e.getExerciseName(),e.getExerciseCaloriesHour())
